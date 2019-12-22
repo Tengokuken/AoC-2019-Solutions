99c601415a3dacf3dc52d53eb23b762e00fcc8b0
Tengokuken <Johnsonz8642@gmail.com>
2019-12-22 00:21:44 -0500

    day 1

---
+++ day1/day1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+public class SpaceModules {
+    static void Main(string[] args) {
+        string[] input = File.ReadAllLines(args[0]);
+        int sum = 0;
+        foreach(string mass in input) {
+            int sumFuel = 0;
+            int req = int.Parse(mass);
+            while (req/3 - 2 > 0) {
+                req = (req/3 - 2);
+                sumFuel += req;
+            }
+            sum += sumFuel;
+        }
+        Console.WriteLine(sum);
+    }
+}
